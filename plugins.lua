2c69ed87cbec026fd583d7687200821c7dd3f1a5
Ben Simmonds <ben@bencode.net>
2023-11-26 21:32:13 +1100

    added fugitive as custom plugin and set some sane general vim config

--- plugins.lua
+++ plugins.lua
@@ -1,18 +1,22 @@
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "rust-analyzer"
-      }
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "rust-analyzer"
+            }
+        }
+    },
+    {
+        "tpope/vim-fugitive",
+        cmd = { "Git", "Gedit", "Gsplit", "Gdiffsplit" }
     }
-  }
 }
 return plugins
