97a98e96b067184f7b0edc636268f0459ca643bb
Ben Simmonds <ben@bencode.net>
2023-11-26 21:59:45 +1100

    configured auto-formatting with null-ls

--- plugins.lua
+++ plugins.lua
@@ -1,22 +1,28 @@
 local plugins = {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "plugins.configs.lspconfig"
-            require "custom.configs.lspconfig"
-        end,
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
     },
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "rust-analyzer"
-            }
-        }
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+      },
     },
-    {
-        "tpope/vim-fugitive",
-        cmd = { "Git", "Gedit", "Gsplit", "Gdiffsplit" }
-    }
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "Gedit", "Gsplit", "Gdiffsplit" },
+  },
 }
 return plugins
